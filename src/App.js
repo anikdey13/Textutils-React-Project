bea7088a6b854951036bb817a9f2f9543cc4db25
Anik Dey <deyanik659@gmail.com>
2023-04-20 19:21:24 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <div className="container">
         <Navbar title="textutils" />
-        <TextForm heading="Give text to analyze" />
+        <TextForm heading="Give text to analyze" /> 
+        {/* <About/> */}
       </div>
     </>
   );
